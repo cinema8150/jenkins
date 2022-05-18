f890f463faf430b76ea5fee700c61ea125028d91
cinema8150 <cinema8150@hotmail.com>
2022-05-18 16:02:58 +0800

    load config file

--- cmd/root.go
+++ cmd/root.go
@@ -5,12 +5,13 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"log"
 	"os"
+
+	"jenkins/service"
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -37,15 +38,15 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.jenkins.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	cobra.OnInitialize(initConfig)
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-
+func initConfig() {
+	_, err := service.LoadConfig()
+	if err != nil {
+		//FIXME: log vs fmt
+		log.Fatalf("load config fatal: %s", err)
+		return
+	}
+}
---
+++ service/config.go
@@ -0,0 +1,52 @@
+package service
+
+import (
+	"os"
+	"path"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func LoadConfig() (string, error) {
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	var (
+		cfgFileFolder = path.Join(home, ".cinema8150")
+		cfgFileName   = ".jenkins"
+		cfgFileType   = "yaml"
+		cfgFilePath   = path.Join(cfgFileFolder, cfgFileName+"."+cfgFileType)
+	)
+
+	cobra.CheckErr(err)
+	viper.SetConfigName(cfgFileName)
+	viper.SetConfigType(cfgFileType)
+	viper.AddConfigPath(cfgFileFolder)
+
+	//FIXME:
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+
+	if _, ok := err.(viper.ConfigFileNotFoundError); ok { // config not found
+
+		os.MkdirAll(cfgFileFolder, os.ModePerm)
+		os.Create(cfgFilePath)
+
+		err = viper.WriteConfig()
+		if err != nil {
+			return "", err
+		}
+	} else if err != nil { // unkonwn err
+		return "", err
+	}
+
+	viper.ConfigFileUsed()
+
+	return cfgFilePath, nil
+
+}
